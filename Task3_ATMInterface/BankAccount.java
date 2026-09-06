6ed802b6ab2eec12418d2b79e99f91f90d3dc3eb
Abhinav Mandal <abhinavkumar21at@gmail.com>
2026-09-06 19:53:07 +0530

    Implement BankAccount class with deposit and withdraw

--- Task3_ATMInterface/BankAccount.java
+++ Task3_ATMInterface/BankAccount.java
@@ -1,1 +1,38 @@
+public class BankAccount {
 
+    double balance;
+
+    BankAccount(double balance) {
+        this.balance = balance;
+    }
+
+    void deposit(double amount) {
+
+        if (amount > 0) {
+            balance = balance + amount;
+            System.out.println("Amount deposited successfully.");
+        }
+        else {
+            System.out.println("Invalid amount.");
+        }
+    }
+
+    void withdraw(double amount) {
+
+        if (amount <= 0) {
+            System.out.println("Invalid amount.");
+        }
+        else if (amount > balance) {
+            System.out.println("Insufficient balance.");
+        }
+        else {
+            balance = balance - amount;
+            System.out.println("Amount withdrawn successfully.");
+        }
+    }
+
+    void checkBalance() {
+
+        System.out.println("Current Balance: Rs. " + balance);
+    }
+}
