7734676113ae64ce924668fa9512da422409ea28
Abhinav Mandal <abhinavkumar21at@gmail.com>
2026-09-06 19:52:01 +0530

    Implement ATM menu functionality

--- Task3_ATMInterface/ATM.java
+++ Task3_ATMInterface/ATM.java
@@ -1,1 +1,51 @@
+import java.util.Scanner;
 
+public class ATM {
+
+    void start(BankAccount account) {
+
+        Scanner sc = new Scanner(System.in);
+        int choice;
+
+        do {
+            System.out.println();
+            System.out.println("----- ATM MENU -----");
+            System.out.println("1. Check Balance");
+            System.out.println("2. Withdraw");
+            System.out.println("3. Deposit");
+            System.out.println("4. Exit");
+
+            System.out.print("Enter your choice: ");
+            choice = sc.nextInt();
+
+            switch (choice) {
+
+                case 1:
+                    account.checkBalance();
+                    break;
+
+                case 2:
+                    System.out.print("Enter amount to withdraw: ");
+                    double withdrawAmount = sc.nextDouble();
+                    account.withdraw(withdrawAmount);
+                    break;
+
+                case 3:
+                    System.out.print("Enter amount to deposit: ");
+                    double depositAmount = sc.nextDouble();
+                    account.deposit(depositAmount);
+                    break;
+
+                case 4:
+                    System.out.println("Thank you for using ATM.");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice.");
+            }
+
+        } while (choice != 4);
+
+        sc.close();
+    }
+}
